98f8a52450f04699afd9551765c54de47bd07e0f
NickMonks <nicolasmg94@gmail.com>
2021-06-30 16:10:48 +0100

    First Version

---
+++ src/app/shared/ingredients.model.ts
@@ -0,0 +1,7 @@
+export class Ingredient {
+
+    // a shortcut can be simply give the access modifier in the constructor , so we dont need to define
+    // the members/properties. 
+    constructor (public name: string, public amount: string){
+    }
+}
